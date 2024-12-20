a311d506a9f6e249df9621caaff141fb2364346d
niraj makwana <niraj@ouiteo.com>
2024-12-20 11:37:45 +0530

    WIP : pytest updated for is_poll_until_complete()

--- tests/test_client_graphql.py
+++ tests/test_client_graphql.py
@@ -1,7 +1,7 @@
 import pytest
 from .utils import generate_opts_and_sess, local_server_session, async_local_server_session
 from basic_shopify_api import Client, AsyncClient
-
+from pytest_httpx._httpx_mock import HTTPXMock
 
 @pytest.mark.usefixtures("local_server")
 @local_server_session
@@ -23,6 +23,7 @@ async def test_graphql_async_return(local_server):
         assert response.body["data"]["shop"]["name"] == "Apple Computers"
         assert response.errors is None
 
+
 @pytest.mark.asyncio
 @pytest.mark.parametrize("max_limit", [(1), (2)])
 @pytest.mark.usefixtures("local_server")
@@ -43,4 +44,17 @@ async def test_is_bulk_job_running(local_server, mock_graphql_response, job_stat
         mock_graphql_response(f"bulk_job_{job_status.lower()}.json")
         response = await c.is_bulk_job_running("QUERY")
         assert response == expected
-        
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("local_server")
+@async_local_server_session
+async def test_poll_until_complete(local_server, mock_graphql_response, httpx_mock: HTTPXMock) -> None:
+    async with AsyncClient(*generate_opts_and_sess()) as c:
+        job_id = "gid://shopify/BulkOperation/1"
+        url = "https://download.com"
+        mocked_response = "Data Downloaded"
+        mock_graphql_response(f"bulk_job_completed.json")
+        httpx_mock.add_response(url=url, text=mocked_response)
+        response = await c.poll_until_complete(job_id, "QUERY")
+        assert response.text == mocked_response
