45900ac6644f44647ebf275f65176abd8f378989
niraj makwana <niraj@ouiteo.com>
2024-12-20 13:00:14 +0530

    pytest updated

--- tests/test_client_graphql.py
+++ tests/test_client_graphql.py
@@ -31,7 +31,7 @@ async def test_graphql_async_return(local_server):
 @pytest.mark.parametrize("max_limit", [(1), (2)])
 @pytest.mark.usefixtures("local_server")
 @async_local_server_session
-async def test_graphql_pagination_data(local_server, mock_graphql_response, max_limit: int) -> None:
+async def test_graphql_pagination_data(local_server, mock_graphql_response: AsyncMock, max_limit: int) -> None:
     async with AsyncClient(*generate_opts_and_sess()) as c:
         mock_graphql_response("product.json")
         response = await c.graphql_call_with_pagination("products", "Query", {}, max_limit)
@@ -42,7 +42,7 @@ async def test_graphql_pagination_data(local_server, mock_graphql_response, max_
 @pytest.mark.parametrize("job_status, expected", [("RUNNING", True), ("COMPLETED", False)])
 @pytest.mark.usefixtures("local_server")
 @async_local_server_session
-async def test_is_bulk_job_running(local_server, mock_graphql_response, job_status: str, expected: bool) -> None:
+async def test_is_bulk_job_running(local_server, mock_graphql_response: AsyncMock, job_status: str, expected: bool) -> None:
     async with AsyncClient(*generate_opts_and_sess()) as c:
         mock_graphql_response(f"bulk_job_{job_status.lower()}.json")
         response = await c.is_bulk_job_running("QUERY")
@@ -52,7 +52,7 @@ async def test_is_bulk_job_running(local_server, mock_graphql_response, job_stat
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("local_server")
 @async_local_server_session
-async def test_poll_until_complete(local_server, mock_graphql_response, httpx_mock: HTTPXMock) -> None:
+async def test_poll_until_complete(local_server, mock_graphql_response: AsyncMock, httpx_mock: HTTPXMock) -> None:
     async with AsyncClient(*generate_opts_and_sess()) as c:
         job_id = "gid://shopify/BulkOperation/1"
         url = "https://download.com"
@@ -67,7 +67,7 @@ async def test_poll_until_complete(local_server, mock_graphql_response, httpx_mo
 @pytest.mark.parametrize("expected_type, wait", [(str, False), (jsonlines.Reader, True)])
 @pytest.mark.usefixtures("local_server")
 @async_local_server_session
-async def test_run_bulk_operation_query(local_server, mock_graphql_response, monkeypatch: pytest.MonkeyPatch, expected_type, wait) -> None:
+async def test_run_bulk_operation_query(local_server, mock_graphql_response: AsyncMock, monkeypatch: pytest.MonkeyPatch, expected_type: type, wait: bool) -> None:
     async with AsyncClient(*generate_opts_and_sess()) as c:
         mock_graphql_response(f"bulk_submit_query.json")
 
@@ -79,3 +79,20 @@ async def test_run_bulk_operation_query(local_server, mock_graphql_response, mon
         
         response = await c.run_bulk_operation_query("query", {}, wait)
         assert isinstance(response, expected_type)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("expected_type, wait", [(str, False), (jsonlines.Reader, True)])
+@pytest.mark.usefixtures("local_server")
+@async_local_server_session
+async def test_run_bulk_operation_mutation(local_server, mock_graphql_response: AsyncMock, monkeypatch: pytest.MonkeyPatch, httpx_mock: HTTPXMock, expected_type: type, wait: bool) -> None:
+    async with AsyncClient(*generate_opts_and_sess()) as c:
+        mock_graphql_response(f"create_and_submit_mutation.json")
+        httpx_mock.add_response(url="https://upload.com", text="Data Uploaded")
+
+        complete_response = AsyncMock(return_value=HttpxResponse(status_code=204, content=b'{"data": {"entity": "data"}}'))
+        monkeypatch.setattr("basic_shopify_api.clients.async_client.AsyncClient.poll_until_complete", complete_response)
+        
+        row_data = [{"field1": "value1", "field2": "value2"}]
+        response = await c.run_bulk_operation_mutation("query", row_data, "input", wait)
+        assert isinstance(response, expected_type)
+        
